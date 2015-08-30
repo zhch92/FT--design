7b2d10967eaf8439b5e3f579c2d8f38d43b776ca
zhch <873384958@qq.com>
2015-08-31 00:21:08 +0800

    show  未修改

--- js/index.js
+++ js/index.js
@@ -8,29 +8,29 @@ $(document).ready(function() {
     // 平滑滚动到相应位置
     $(".home").click(function() {
         $.scrollTo('.floor_1', 500);
-        $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+        // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
     });
     $(".product").click(function() {
-        $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
-        $(".floor_2").css("padding-top", '87px');
+        // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+        // $(".floor_2").css("padding-top", '87px');
         $.scrollTo('.floor_2', 500);
 
     });
     $(".awards").click(function() {
-        $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
-        $(".floor_5").css("padding-top", '87px');
+        // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+        // $(".floor_5").css("padding-top", '87px');
         $.scrollTo('.floor_5', 500);
 
     });
     $(".about").click(function() {
-        $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
-        $(".floor_6").css("padding-top", '87px');
+        // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+        // $(".floor_6").css("padding-top", '87px');
         $.scrollTo('.floor_6', 500);
 
     });
     $(".contact").click(function() {
-        $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
-        $(".floor_7").css("padding-top", '87px');
+        // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+        // $(".floor_7").css("padding-top", '87px');
         $.scrollTo('.floor_7', 500);
 
     });
@@ -88,7 +88,33 @@ function moreShow() {
 };
 
 function scTop() {
-    $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
-    $(".floor_2").css("padding-top", '87px');
+    // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+    // $(".floor_2").css("padding-top", '87px');
     $.scrollTo('.floor_2', 500);
 }
+
+function productShow(val, cls) {
+    var pdt = '<div class=\"product_spread\"><div class=\"spread_content \"><div class=\"clearfix\"><h1>PRODUCT</h1><div class=\"explain\"><p class=\"title\">SDFDSFF</p><p class=\"introduce\">KDJSFJOSDOFIJDSOJFODSO      OJDFOGIJDODFJGDFIG   DJSOGISDSDVDSVFDS</p></div></div><div class=\"product_show\"><a class=\"cancel\" href=\"javascript:void(0);\"></a><a class=\"pre\" href=\"javascript:void(0);\"></a><a class=\"next\" href=\"javascript:void(0);\"></a><img src=\"images/product.jpg\" alt=\"\"></div></div></div>';
+    var pdtPostition = $('.' + cls).next().position().top;
+    var pdtHeight=$('.product_spread').height()+"px";
+    if (val == 0) {
+        alert("0")
+        $('.' + cls).before(pdt);
+        $('.product_spread').css('top', pdtPostition);
+        $(".floor_4 .item").animate({
+            top: pdtHeight
+        }, 1000)
+    }
+    if (val == 1) {
+        $('.' + cls).after(pdt);
+        $('.product_spread').css('top', pdtPostition);
+
+        $(".product_spread").nextAll().animate({
+            top: pdtHeight
+        }, 1000)
+    };
+
+}
+
+
+
