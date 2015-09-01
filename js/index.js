61078678e619e5e1e7f61509afbd0d53518ffe48
friday <2055472983@qq.com>
2015-09-01 18:05:28 +0800

    修改多图位置，响应式布局

--- js/index.js
+++ js/index.js
@@ -57,64 +57,66 @@ $(document).ready(function() {
         // };
         if (sTop < oF2) {
             $(".header").css('background', "transparent");
-            $(".header .head ul .home").addClass("active").siblings().removeClass("active");
-
+            headChange('home');
         } else {
             $(".header").css('background', "#030303");
         };
         if (sTop > oF2 && sTop < oF5) {
-
-            $(".header .head ul .product").addClass("active").siblings().removeClass("active");
-
+              headChange('product');
         }
         if (sTop > oF5 && sTop < oF6) {
-            $(".header .head ul .awards").addClass("active").siblings().removeClass("active");
-
+            headChange('awards');
         }
         if (sTop > oF6 && sTop < oF7) {
-            $(".header .head ul .about").addClass("active").siblings().removeClass("active");
-
+            headChange('about');
         }
         if (sTop > oF7) {
-            $(".header .head ul .contact").addClass("active").siblings().removeClass("active");
-
+            headChange('contact');
         }
     });
 });
-
+function headChange(cls){
+    $(".header .head ul "+'.'+cls).addClass("active").siblings().removeClass("active");
+}
 function moreShow() {
     $(".floor_2 .more").toggleClass('more_active');
     $("#more").slideToggle();
 };
 
 function scTop() {
-    // $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
-    // $(".floor_2").css("padding-top", '87px');
+    $(".floor_2,.floor_5,.floor_6,.floor_7").css("padding-top", '0px');
+    $(".floor_2").css("padding-top", '87px');
     $.scrollTo('.floor_2', 500);
-}
+};
 
-function productShow(val, cls) {
-    var pdt = '<div class=\"product_spread\"><div class=\"spread_content \"><div class=\"clearfix\"><h1>PRODUCT</h1><div class=\"explain\"><p class=\"title\">SDFDSFF</p><p class=\"introduce\">KDJSFJOSDOFIJDSOJFODSO      OJDFOGIJDODFJGDFIG   DJSOGISDSDVDSVFDS</p></div></div><div class=\"product_show\"><a class=\"cancel\" href=\"javascript:void(0);\"></a><a class=\"pre\" href=\"javascript:void(0);\"></a><a class=\"next\" href=\"javascript:void(0);\"></a><img src=\"images/product.jpg\" alt=\"\"></div></div></div>';
-    var pdtPostition = $('.' + cls).next().position().top;
-    var pdtHeight=$('.product_spread').height()+"px";
-    if (val == 0) {
-        alert("0")
-        $('.' + cls).before(pdt);
-        $('.product_spread').css('top', pdtPostition);
-        $(".floor_4 .item").animate({
-            top: pdtHeight
-        }, 1000)
-    }
-    if (val == 1) {
-        $('.' + cls).after(pdt);
-        $('.product_spread').css('top', pdtPostition);
-
-        $(".product_spread").nextAll().animate({
-            top: pdtHeight
+function productShow(val) {
+removeShow();
+    var pdt = '<div class=\"product_spread\"><div class=\"spread_content \"><div class=\"clearfix\"><h1>PRODUCT</h1><div class=\"explain\"><p class=\"title\">SDFDSFF</p><p class=\"introduce\">KDJSFJOSDOFIJDSOJFODSO      OJDFOGIJDODFJGDFIG   DJSOGISDSDVDSVFDS</p></div></div><div class=\"product_show\"><a class=\"cancel\" href=\"javascript:void(0);\" onclick=\"removeShow()\"></a><a class=\"pre\" href=\"javascript:void(0);\"></a><a class=\"next\" href=\"javascript:void(0);\"></a><img src=\"images/product.jpg\" alt=\"\"></div></div></div>';
+    // var pdtPostition = $('.' + cls).next().position().top;
+    // var pdtHeight=$('.product_spread').height()+"px";
+    if (val==0) {
+        $('.product_show_1').before(pdt);
+        $('.product_spread').animate({
+            height: '884px'
         }, 1000)
     };
-
+      if (val==1) {
+        $('.product_show_2').before(pdt);
+        $('.product_spread').animate({
+            height: '884px'
+        }, 1000)
+    };
+};
+function removeShow(){
+    $('.product_spread').animate({
+            height: '0px'
+        }, 500,function(){
+              $('.product_spread').remove();    
+        })
+      
 }
+
+
 
 
 
